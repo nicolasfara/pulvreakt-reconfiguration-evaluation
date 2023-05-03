bae81ede4be0bbf463cb7a49f726a49e6cf1a58c
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-05-03 12:06:17 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("com.gradle.enterprise") version "3.13"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+rootProject.name = "alchemist-experiments-bootstrap"
